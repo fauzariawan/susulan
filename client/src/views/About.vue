38d3ed77481d2199a7b6cdfac16cf898d1d4949a
fauzariawan <umardastan2014@gmail.com>
2020-11-18 17:55:48 +0700

    git add git commit terakhir

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,16 +1,67 @@
 <template>
   <div>
     <h1>ini halaman utama</h1>
+    <div class="card">
+      <div class="cardCategori">{{ randomJoke.category }}</div>
+      <div class="cardSetup">{{ randomJoke.setup }}</div>
+      <div class="cardDelivery">{{ randomJoke.delivery }}</div>
+      <br />
+      <div class="button">
+        <button @click.prevent="addToFavourite(randomJoke.id)">
+          Add to Favourite
+        </button>
+      </div>
+    </div>
+    <div></div>
   </div>
 </template>
 
 <script>
+import axios from "../../axios/axiosInstance";
+
 export default {
+  name: "About",
+  data() {
+    return {
+      randomJoke: {},
+    };
+  },
+  methods: {
+    fetchRandomJoke() {
+      const token = localStorage.token;
+      console.log(token);
+      axios({
+        url: "/jokes/random",
+        method: "get",
+        headers: { token },
+      })
+        .then(({ data }) => {
+          this.randomJoke = data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    addToFavourite(id) {
+      const token = localStorage.token;
+      axios({
+        url: "/favourites",
+        method: "post",
+        headers: { token },
+        data: {
+          JokeId: id,
+        },
+      })
+        .then(() => {})
+        .catch((error) => {});
+    },
+  },
   created() {
     const token = localStorage.token;
     if (!token) {
-      this.$router.push({ name: "" });
+      this.$router.push({ name: "Login" });
     } else {
+      this.fetchRandomJoke();
     }
   },
 };
--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,14 +1,45 @@
 <template>
   <div class="home">
-    <div>ini halaman utuk login</div>
+    <form @submit.prevent="login">
+      <label for="">username</label>
+      <input v-model="username" type="text" name="username" id="username" />
+      <label for="">password</label>
+      <input v-model="password" ltype="text" name="password" id="password" />
+      <button type="submit">Login</button>
+    </form>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import axios from "../../axios/axiosInstance";
 export default {
   name: "Login",
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      axios({
+        url: "/signIn",
+        method: "post",
+        data: {
+          username: this.username,
+          password: this.password,
+        },
+      })
+        .then(({ data }) => {
+          localStorage.setItem("token", data.token);
+          this.$router.push({ name: "About" });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
   components: {},
 };
 </script>
